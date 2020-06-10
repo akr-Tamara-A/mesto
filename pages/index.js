20000835aaf9d505a180ade7ac5fac179266e6b4
Tamara A <akira-49-q@yandex.ru>
2020-06-10 22:46:37 +0300

    feat: реализовать редактирование профиля

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-let popup = document.querySelector('.popup');
-let popupOpen = document.querySelector('.button__edit-profile');
-let popupClose = document.querySelector('.popup__close');
-
+const popup = document.querySelector('.popup');
+const popupOpen = document.querySelector('.button__edit-profile');
+const popupClose = document.querySelector('.popup__close');
 
 function togglePopup() {
   popup.classList.toggle('popup__opened');
@@ -11,13 +10,30 @@ popupOpen.addEventListener('click', togglePopup);
 popupClose.addEventListener('click', togglePopup);
 
 
-console.log(`${popup.classList}`);
 
+let formSubmit = document.querySelector('.popup__submit');
 
+function formSubmitHandler (evt) {
+  evt.preventDefault(); 
+  
+  let nameInput = document.querySelector('.popup__username')
+  let jobInput = document.querySelector('.popup__about')
 
+  let newUserName = nameInput.value;
+  let newUserJob = jobInput.value;
 
+  let userName = document.querySelector('.profile__user-name');
+  let userJob = document.querySelector('.profile__user-job');
 
+  console.log(nameInput, jobInput);
+  console.log(userName, userJob);
+  console.log(newUserName, newUserJob);
 
+  userName.textContent = newUserName;
+  userJob.textContent = newUserJob;
+}
+
+formSubmit.addEventListener('submit', formSubmitHandler);
 
 
 
