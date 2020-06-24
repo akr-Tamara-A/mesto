0ec92891257a59bbb3ceee9c0a8a3ac814b5cf12
Tamara A <akira-49-q@yandex.ru>
2020-06-24 19:15:01 +0300

    feat: реализовать открытие и закрытия двух попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,78 @@
-let popup = document.querySelector('.popup');
-let popupOpenButton = document.querySelector('.button_type_edit-profile');
-let popupCloseButton = document.querySelector('.popup__close');
+let editProfilePopup = document.querySelector('#popupEditProfile');
+let addFotoPopup = document.querySelector('#popupAddFoto');
+
+let editProfileOpenButton = document.querySelector('.button_type_edit-profile');
+let addFotoOpenButton = document.querySelector('.button_type_add-foto');
 
 let userName = document.querySelector('.profile__user-name');
 let userJob = document.querySelector('.profile__user-job');
 
-let formPopup = document.querySelector('.popup__container');
 let formUserName = document.querySelector('.popup__input_type_username');
 let formUserJob = document.querySelector('.popup__input_type_about');
-let formSubmit = document.querySelector('.popup__submit');
+let formFotoTitle = document.querySelector('.popup__input_type_foto-title');
+let formFotoLink = document.querySelector('.popup__input_type_foto-link');
 
 
-// Открытие редактирования профиля пользователя
-function openPopup() {
+// Открытие модального окна
+function openPopup(popup) {
   popup.classList.add('popup_opened');
+  popup.classList.add('popup_fade_in');
+}
+
+
+// Закрытие модального окна
+function closePopup(popup) {
+  popup.classList.remove('popup_fade_in');
+  popup.classList.add('popup_fade_out');
+  popup.classList.remove('popup_opened');
+}
+
+
+// Открытие окна редактирования профиля пользователя
+editProfileOpenButton.addEventListener('click', openEditProfilePopup);
+
+function openEditProfilePopup() {
+  openPopup(editProfilePopup);
   formUserName.focus();
   formUserName.value = userName.textContent;
   formUserJob.value = userJob.textContent;
 }
 
-popupOpenButton.addEventListener('click', openPopup);
 
+// Закрытие окна редактирования профиля пользователя
+editProfilePopup.querySelector('.popup__close').addEventListener('click', closeEditProfilePopup);
 
-// Закрытие редактирования профиля пользователя
-function closePopup() {
-  popup.classList.remove('popup_opened');
-  popupOpenButton.focus();
+function closeEditProfilePopup() {
+  closePopup(editProfilePopup);
+  editProfileOpenButton.focus();
 }
-
-popupCloseButton.addEventListener('click', closePopup);
 
 
 // Отмена стандартной отправки формы и замена данных профиля пользователя
-function newValuePopup(evt) {
+editProfilePopup.querySelector('.popup__submit').addEventListener('click', newValueEditProfile);
+
+function newValueEditProfile(evt) {
   evt.preventDefault(); 
   userName.textContent = formUserName.value;
   userJob.textContent = formUserJob.value;
   closePopup();
 }
 
-formSubmit.addEventListener('click', newValuePopup);
+
+// Открытие окна добавления фото
+addFotoOpenButton.addEventListener('click', openAddFotoPopup);
+
+function openAddFotoPopup() {
+  openPopup(addFotoPopup);
+  formFotoTitle.focus();
+}
+
+
+// Закрытие окна добавления фото
+addFotoPopup.querySelector('.popup__close').addEventListener('click', closeAddFotoPopup);
+
+function closeAddFotoPopup() {
+  closePopup(addFotoPopup);
+  editProfileOpenButton.focus();
+}
 
