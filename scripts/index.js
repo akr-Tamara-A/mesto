17519f2db5e2b5c5b53333267be74cab21a810b7
Tamara A <akira-49-q@yandex.ru>
2020-06-25 16:31:10 +0300

    feat: добавить обработку модальных окон

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,23 @@
-let editProfilePopup = document.querySelector('#popupEditProfile');
-let addFotoPopup = document.querySelector('#popupAddFoto');
+const editProfilePopup = document.querySelector('#popupEditProfile');
+const addFotoPopup = document.querySelector('#popupAddFoto');
+const viewFotoPopup = document.querySelector('#popupViewFoto');
 
-let editProfileOpenButton = document.querySelector('.button_type_edit-profile');
-let addFotoOpenButton = document.querySelector('.button_type_add-foto');
+const editProfileOpenButton = document.querySelector('.button_type_edit-profile');
+const addFotoOpenButton = document.querySelector('.button_type_add-foto');
 
-let userName = document.querySelector('.profile__user-name');
-let userJob = document.querySelector('.profile__user-job');
+const userName = document.querySelector('.profile__user-name');
+const userJob = document.querySelector('.profile__user-job');
 
-let formUserName = document.querySelector('.popup__input_type_username');
-let formUserJob = document.querySelector('.popup__input_type_about');
-let formFotoTitle = document.querySelector('.popup__input_type_foto-title');
-let formFotoLink = document.querySelector('.popup__input_type_foto-link');
+const formUserName = editProfilePopup.querySelector('.popup__input_type_username');
+const formUserJob = editProfilePopup.querySelector('.popup__input_type_about');
 
+const formFotoTitle = addFotoPopup.querySelector('.popup__input_type_foto-title');
+const formFotoLink = addFotoPopup.querySelector('.popup__input_type_foto-link');
+
+const popupFotoLink = viewFotoPopup.querySelector('.popup__foto');
+const popupFotoTitle = viewFotoPopup.querySelector('.popup__foto-title');
+
+const elementContainer = document.querySelector('.elements__container');
 
 // Открытие модального окна
 function openPopup(popup) {
@@ -55,7 +61,77 @@ function newValueEditProfile(evt) {
   evt.preventDefault(); 
   userName.textContent = formUserName.value;
   userJob.textContent = formUserJob.value;
-  closePopup();
+  closeEditProfilePopup();
+}
+
+
+// Шесть карточек «из коробки»
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]
+
+// Открытие модального окна просмотра полноразмерного фото
+function openViewFoto(elem) {
+  elem.querySelector('.element__image').addEventListener('click', function(evt) {
+    const elementFoto = evt.target;
+    const element = elementFoto.closest('.element');
+    
+    const elementTitle = element.querySelector('.element__title');
+    const elementLink = element.querySelector('.element__image');
+
+    openPopup(viewFotoPopup);
+
+    popupFotoLink.src = elementLink.src;
+    popupFotoTitle.textContent = elementTitle.textContent;
+  })
+}
+
+
+// Закрытие окна просмотра полноразмерного фото
+viewFotoPopup.querySelector('.popup__close').addEventListener('click', closeViewFotoPopup);
+function closeViewFotoPopup() {
+  closePopup(viewFotoPopup);
+}
+
+
+// Обработка кнопки like
+function buttonLike(elem) {
+  elem.querySelector('.element__like').addEventListener('click', function(evt) {
+    evt.target.classList.toggle('button_type_like');
+  })
+}
+
+
+// Удаление карточки
+function deleteElement(elem) {
+  elem.querySelector('.button_type_delete').addEventListener('click', function(evt) {
+    const deleteButton = evt.target;
+    const element = deleteButton.closest('.element');
+    element.remove();
+  })
 }
 
 
@@ -73,6 +149,59 @@ addFotoPopup.querySelector('.popup__close').addEventListener('click', closeAddFo
 
 function closeAddFotoPopup() {
   closePopup(addFotoPopup);
+  formFotoTitle.value = "";
+  formFotoLink.value = "";
   editProfileOpenButton.focus();
 }
+
+
+// Добавление карточек на страницу
+initialCards.forEach(function(elem) {
+  const cardTemplate = document.querySelector('#elementTemplate').content;
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.element__image').src = elem.link;
+  cardElement.querySelector('.element__image').alt = elem.name;
+
+  cardElement.querySelector('.element__title').title = elem.name;
+  cardElement.querySelector('.element__title').textContent = elem.name;
+
+  openViewFoto(cardElement);
+  
+  buttonLike(cardElement);
+
+  deleteElement(cardElement);
+  
+  elementContainer.append(cardElement);
+})
+
+
+// Добавление карточки на страницу пользователем
+addFotoPopup.querySelector('.popup__submit').addEventListener('click', function(evt) {
+  evt.preventDefault(); 
+  addFoto(formFotoTitle.value, formFotoLink.value)
+});
+
+function addFoto (fotoTitleValue, fotoLinkValue) {
+  const cardTemplate = document.querySelector('#elementTemplate').content;
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.element__image').src = fotoLinkValue;
+  cardElement.querySelector('.element__image').alt = fotoTitleValue;
+
+  cardElement.querySelector('.element__title').title = fotoTitleValue;
+  cardElement.querySelector('.element__title').textContent = fotoTitleValue;
+
+  openViewFoto(cardElement);
+
+  buttonLike(cardElement);
+
+  deleteElement(cardElement);
+
+  elementContainer.prepend(cardElement);
+
+  closeAddFotoPopup();
+}
+
+
 
