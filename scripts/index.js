3d9f110b9ae0c2b04b2fde666813596212a0577a
Tamara A <akira-49-q@yandex.ru>
2020-06-26 17:56:09 +0300

    feat: изменить выбор фото

--- scripts/index.js
+++ scripts/index.js
@@ -36,8 +36,6 @@ function closePopup(popup) {
 
   setTimeout(function() {popup.classList.remove('popup_opened')}, 400);
   setTimeout(function() {popup.classList.remove('popup_fade_out')}, 400);
-  //popup.classList.remove('popup_fade_out');
- 
 }
 
 
@@ -75,28 +73,28 @@ function newValueEditProfile(evt) {
 // Шесть карточек «из коробки»
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Северный Ледовитый океан',
+    link: 'https://cdn.pixabay.com/photo/2019/03/27/18/53/arctic-ocean-4085638_960_720.jpg'
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: 'Волга',
+    link: 'https://cdn.pixabay.com/photo/2020/05/28/17/20/russia-5231942_960_720.jpg'
   },
   {
     name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    link: 'https://cdn.pixabay.com/photo/2018/11/16/10/10/baikal-3819068_960_720.jpg'
+  },
+  {
+    name: 'Ольхон',
+    link: 'https://cdn.pixabay.com/photo/2018/12/18/16/47/olkhon-3882674_960_720.jpg'
+  },
+  {
+    name: 'Каспийское море',
+    link: 'https://cdn.pixabay.com/photo/2017/12/21/23/14/caspian-sea-3032750_960_720.jpg'
+  },
+  {
+    name: 'Енисей',
+    link: 'https://cdn.pixabay.com/photo/2012/12/21/10/06/clouds-71498_960_720.jpg'
   }
 ]
 
